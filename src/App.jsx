1729dc4df084e08c986684dee3eb12520912e734
Niloyan Sellathurai <124621796+NiloyanSellathurai02@users.noreply.github.com>
2024-02-24 07:55:00 +0100

    did some changes in the components with state and props

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import {useState} from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
   const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurn = [
+        {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+        ...prevTurns,
+        //
+      ];
+
+      return updatedTurn;
+    });
   }
 
   return (
@@ -17,8 +34,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} turns={gameTurns} />
       </div>
+      <Log />
     </main>
   );
 }
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,3 +1,3 @@
 export default function Log() {
-  return;
+  return <ol id="log"></ol>;
 }
